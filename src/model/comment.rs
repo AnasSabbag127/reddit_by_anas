f1b4e1becaf5e17d23c3389f1ff73acdf04ee425
Anas-Sabbag <anas.sabbag@remotestate.com>
2023-07-24 17:37:57 +0530

    user model added

---
+++ src/model/comment.rs
@@ -0,0 +1,8 @@
+use uuid::Uuid;
+
+pub struct Comments{
+   pub comment_id: Uuid,
+   pub comments: String
+}
+
+
---
+++ src/model/post_info.rs
@@ -0,0 +1,9 @@
+use uuid::Uuid;
+
+
+
+pub struct PostInfo{
+    pub post_info_id: Uuid,
+    pub post_title: String,
+    pub post_info: String
+}
